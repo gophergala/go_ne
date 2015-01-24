8f2eae172056bc16ee6bf804687e8151bb44b632
Tobias Haar <tobias.haar@googlemail.com>
2015-01-24 17:21:38 +0000

    extracted running logic into core

--- core/task.go
+++ core/task.go
@@ -1,6 +1,53 @@
 package core
 
+import (
+	"log"
+	"os"
+
+	"github.com/gophergala/go_ne/plugins/core"
+)
+
 type Task interface {
 	Name() string
 	Args() []string
+}
+
+func RunAll(runner Runner, config *Config) {
+	var err error
+	var task Task
+
+	for _, t := range config.Tasks {
+		for _, s := range t.Steps {
+			if s.Plugin != nil {
+				// Load plugin
+				p, err := GetPlugin(*s.Plugin)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+
+				pluginArgs := plugin.Args{
+					Environment: os.Environ(),
+					Options:     s.Args,
+				}
+
+				task, err = p.GetCommand(pluginArgs)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+			} else {
+				// Run arbitrary command
+				task, err = NewCommand(*s.Command, s.Args)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+			}
+
+			runner.Run(task)
+		}
+	}
+
+	StopAllPlugins()
 }
--- daemon/main.go
+++ daemon/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/gophergala/go_ne/core"
-	"github.com/gophergala/go_ne/plugins/core"
 )
 
 // BUG(Tobscher) use command line arguments to perform correct task
@@ -24,39 +22,5 @@ func main() {
 
 	runner, _ := NewLocalRunner()
 
-	var task core.Task
-	for _, t := range config.Tasks {
-		for _, s := range t.Steps {
-			if s.Plugin != nil {
-				// Load plugin
-				p, err := core.GetPlugin(*s.Plugin)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-
-				pluginArgs := plugin.Args{
-					Environment: os.Environ(),
-					Options:     s.Args,
-				}
-
-				task, err = p.GetCommand(pluginArgs)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-			} else {
-				// Run arbitrary command
-				task, err = core.NewCommand(*s.Command, s.Args)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-			}
-
-			runner.Run(task)
-		}
-	}
-
-	core.StopAllPlugins()
+	core.RunAll(runner, config)
 }
