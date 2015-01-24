70da1addce19bd1204617442091a28a760c904f2
Tobias Haar <tobias.haar@googlemail.com>
2015-01-24 14:57:30 +0000

    Added log for each step

--- daemon/local.go
+++ daemon/local.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
 
 	"github.com/gophergala/go_ne/core"
 )
@@ -17,10 +18,13 @@ func NewLocalRunner() (*Local, error) {
 }
 
 func (l *Local) Run(task core.Task) {
+	log.Printf("Running task: %v %v\n", task.Name(), strings.Join(task.Args(), " "))
+
 	cmd := exec.Command(task.Name(), task.Args()...)
 	var out bytes.Buffer
 	cmd.Stdout = &out
-	err := cmd.Run(); if err != nil {
+	err := cmd.Run()
+	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(out.String())
